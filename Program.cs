8835d46ba3d54fe0973c96acdcf494eda15a8e7a
John Nordberg <john.nordberg@gmail.com>
2022-04-10 23:16:47 +0300

    refactoring

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace UusiaAstia
 {
+	using System;
 	using System.Dynamic;
 	using System.Net.Http;
 	using System.Net.Http.Json;
@@ -34,69 +35,81 @@
 
 	public class Astia
 	{
+		private HttpClient http = new();
+
 		public async Task GetNewUrl(string kuid)
 		{
-			HttpClient http = new();
+			//kuid = "7137029";
+			//var jaksoAndtunniste = await GetJaksoAndTunniste(kuid);
+			var jaksoAndtunniste = new JaksoAndTunniste { at3_ay_tunnus = "1294995.KA", ayid = 114889, jakso = 102 };
 
-			// var response = await http.GetAsync($"http://digi.narc.fi/fetchJaksoAndTunniste.php?kuid={kuid}");
+			//var id = await GetAineistoId(jaksoAndtunniste);
+			var id = "1193635722";
 
-			// JaksoAndTunniste jaksoAndTunniste = new();
-			// if (response.IsSuccessStatusCode)
-			// {
-			// 	jaksoAndTunniste = await response.Content.ReadFromJsonAsync<JaksoAndTunniste>() ?? new JaksoAndTunniste();
-			// 	//Console.WriteLine(jaksoAndTunniste?.ToString());
-			// }
-			// else
-			// {
-			// 	string error = await response.Content.ReadAsStringAsync();
-			// 	Console.WriteLine(error);
-			// 	return;
-			// }
-
-			// TietoQuery tietoQuery = new() { searchString = $"AY_{jaksoAndTunniste?.at3_ay_tunnus}", searchTarget = "aineisto" };
-
-			// response = await http.PostAsJsonAsync("https://astia.narc.fi/uusiastia/aineisto/read.php", tietoQuery);
-			// string json = string.Empty;
-			// if (response.IsSuccessStatusCode)
-			// {
-			// 	//aineistoResult = await response.Content.ReadFromJsonAsync<ExpandoObject>() ?? new ExpandoObject();
-			// 	json = await response.Content.ReadAsStringAsync();
-			// 	//File.WriteAllText("aineistoResult.json", json);
+			string tiedosto = await GetTiedosto(jaksoAndtunniste, id);
 
 
-			// }
-			// else
-			// {
-			// 	string result = await response.Content.ReadAsStringAsync();
-			// 	Console.WriteLine(result);
-			// 	return;
-			// }
+		}
 
-			// string json = File.ReadAllText("aineistoResult.json");
-
-			// dynamic aineistoResult = JsonObject.Parse(json);
-			// JsonArray tulokset = aineistoResult["tulokset"];
-			// dynamic aineisto = tulokset[0];
-			// dynamic id = aineisto["id"];
-
-			// Console.WriteLine(id);
-			string id = "1193635722";
+		private async Task<string> GetTiedosto(JaksoAndTunniste jaksoAndtunniste, string id)
+		{
 			var response = await http.GetAsync($"https://astia.narc.fi/uusiastia/json/json_tiedostot.php?id={id}");
 			string json = string.Empty;
 			if (response.IsSuccessStatusCode)
 			{
 				var tiedostoResult = await response.Content.ReadFromJsonAsync<ExpandoObject>() ?? new ExpandoObject();
 				json = await response.Content.ReadAsStringAsync();
-				File.WriteAllText("tiedostoResult.json", json);
 			}
 			else
 			{
-				string result = await response.Content.ReadAsStringAsync();
-				Console.WriteLine(result);
-				return;
+				string error = await response.Content.ReadAsStringAsync();
+				throw new Exception(error);
 			}
 
+			return "";
 
+		}
+
+		private async Task<string> GetAineistoId(JaksoAndTunniste jaksoAndTunniste)
+		{
+			TietoQuery tietoQuery = new() { searchString = $"AY_{jaksoAndTunniste?.at3_ay_tunnus}", searchTarget = "aineisto" };
+
+			var response = await http.PostAsJsonAsync("https://astia.narc.fi/uusiastia/aineisto/read.php", tietoQuery);
+			string json = string.Empty;
+			if (response.IsSuccessStatusCode)
+			{
+				json = await response.Content.ReadAsStringAsync();
+			}
+			else
+			{
+				string error = await response.Content.ReadAsStringAsync();
+				throw new Exception(error);
+
+			}
+
+			dynamic aineistoResult = JsonObject.Parse(json);
+			JsonArray tulokset = aineistoResult["tulokset"];
+			dynamic aineisto = tulokset[0];
+			dynamic id = aineisto["id"];
+			return (string)id;
+		}
+
+		private async Task<JaksoAndTunniste> GetJaksoAndTunniste(string kuid)
+		{
+			var response = await http.GetAsync($"http://digi.narc.fi/fetchJaksoAndTunniste.php?kuid={kuid}");
+
+			JaksoAndTunniste jaksoAndTunniste = new();
+			if (response.IsSuccessStatusCode)
+			{
+				jaksoAndTunniste = await response.Content.ReadFromJsonAsync<JaksoAndTunniste>() ?? new JaksoAndTunniste();
+			}
+			else
+			{
+				string error = await response.Content.ReadAsStringAsync();
+				throw new Exception(error);
+			}
+
+			return jaksoAndTunniste;
 		}
 	}
 }
