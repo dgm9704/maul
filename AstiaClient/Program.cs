cec17c8932db66549b3397f9720fcdf1bd91b374
John Nordberg <john.nordberg@gmail.com>
2022-04-11 01:56:10 +0300

    Functionally complete

--- AstiaClient/Program.cs
+++ AstiaClient/Program.cs
@@ -7,12 +7,10 @@
 	{
 		public static async Task Main(string[] args)
 		{
-			//var oldUrl = args[0];
-			var oldUrl = "http://digi.narc.fi/digi/view.ka?kuid=7137029";
-
+			var oldUrl = args[0];
 			var astia = new Astia();
-
 			var newUrl = await astia.GetNewUrl(oldUrl);
+			Console.WriteLine(newUrl);
 		}
 	}
 }
