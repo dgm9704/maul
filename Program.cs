282e54f6d2cd270646d55deccf4dd9cdff5d0b68
John Nordberg <john.nordberg@gmail.com>
2022-04-10 15:42:08 +0300

    foo

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿namespace UusiaAstia
 {
+	using System.Dynamic;
 	using System.Net.Http;
 	using System.Net.Http.Json;
+	using System.Text.Json.Nodes;
 
 	public class Program
 	{
 		public static async Task Main(string[] args)
 		{
-			var oldUrl = args[0];
+			var oldUrl = "http://digi.narc.fi/digi/view.ka?kuid=7137029"; //args[0];
 			Console.WriteLine(oldUrl);
 			var kuid = oldUrl.Split('=').Last();
 			Console.WriteLine(kuid);
@@ -16,7 +18,7 @@
 		}
 	}
 
-	public class Foo
+	public class JaksoAndTunniste
 	{
 		public int ayid { get; set; }
 		public string at3_ay_tunnus { get; set; } = string.Empty;
@@ -24,7 +26,7 @@
 		public int jakso { get; set; }
 	}
 
-	public class Bar
+	public class TietoQuery
 	{
 		public string searchString { get; set; } = string.Empty;
 		public string searchTarget { get; set; } = string.Empty;
@@ -34,24 +36,50 @@
 	{
 		public async Task GetNewUrl(string kuid)
 		{
-			HttpClient http = new();
+			// HttpClient http = new();
 
-			var response = await http.GetAsync($"http://digi.narc.fi/fetchJaksoAndTunniste.php?kuid={kuid}");
+			// var response = await http.GetAsync($"http://digi.narc.fi/fetchJaksoAndTunniste.php?kuid={kuid}");
 
-			Foo foo = new();
-			if (response.IsSuccessStatusCode)
-			{
-				foo = await response.Content.ReadFromJsonAsync<Foo>() ?? new Foo();
-				Console.WriteLine(foo?.ToString());
-			}
-			else
-			{
-				string error = await response.Content.ReadAsStringAsync();
-				Console.WriteLine(error);
-				return;
-			}
+			// JaksoAndTunniste jaksoAndTunniste = new();
+			// if (response.IsSuccessStatusCode)
+			// {
+			// 	jaksoAndTunniste = await response.Content.ReadFromJsonAsync<JaksoAndTunniste>() ?? new JaksoAndTunniste();
+			// 	//Console.WriteLine(jaksoAndTunniste?.ToString());
+			// }
+			// else
+			// {
+			// 	string error = await response.Content.ReadAsStringAsync();
+			// 	Console.WriteLine(error);
+			// 	return;
+			// }
 
-			Bar bar = new() { searchString = $"AY_{foo?.at3_ay_tunnus}", searchTarget = "aineisto", };
+			// TietoQuery tietoQuery = new() { searchString = $"AY_{jaksoAndTunniste?.at3_ay_tunnus}", searchTarget = "aineisto" };
+
+			// response = await http.PostAsJsonAsync("https://astia.narc.fi/uusiastia/aineisto/read.php", tietoQuery);
+			// string json = string.Empty;
+			// if (response.IsSuccessStatusCode)
+			// {
+			// 	//aineistoResult = await response.Content.ReadFromJsonAsync<ExpandoObject>() ?? new ExpandoObject();
+			// 	json = await response.Content.ReadAsStringAsync();
+			// 	//File.WriteAllText("aineistoResult.json", json);
+
+
+			// }
+			// else
+			// {
+			// 	string result = await response.Content.ReadAsStringAsync();
+			// 	Console.WriteLine(result);
+			// 	return;
+			// }
+
+			string json = File.ReadAllText("aineistoResult.json");
+
+			dynamic aineistoResult = JsonObject.Parse(json);
+			JsonArray tulokset = aineistoResult["tulokset"];
+			dynamic aineisto = tulokset[0];
+			dynamic id = aineisto.id;
+
+			Console.WriteLine(id);
 		}
 	}
 }
