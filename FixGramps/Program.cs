9e7526ea21fd76419b37f861fa23badf1bd630af
John Nordberg <john.nordberg@gmail.com>
2022-04-18 12:38:21 +0300

    Add link styling

--- FixGramps/Program.cs
+++ FixGramps/Program.cs
@@ -7,16 +7,16 @@ namespace FixGramps
 		public static void Main(string[] args)
 		{
 			var inputFile = args[0];
-			var outputFile = Path.ChangeExtension(inputFile, "out");
+			var outputFile = inputFile.Replace(".gramps", "_out.gramps");
 			var doc = XDocument.Load(inputFile);
 			var modified = AddAstiaLinks(doc);
 			modified.Save(outputFile);
 		}
-
-		public static XNamespace gramps = "http://gramps-project.org/xml/1.7.1/";
-
 		public static XDocument AddAstiaLinks(XDocument document)
 		{
+			var gramps = document.Root?.GetDefaultNamespace()
+				?? "http://gramps-project.org/xml/1.7.1/";
+
 			var astia = new Astia.Astia();
 			var notes =
 				document.
@@ -34,7 +34,23 @@ namespace FixGramps
 				var text = note.Descendants(gramps + "text").First();
 				var oldUrl = text.Value;
 				var newUrl = astia.GetNewUrl(oldUrl).Result;
-				text.Value += Environment.NewLine + newUrl;
+
+				text.Value += Environment.NewLine;
+				var start = text.Value.Length;
+				text.Value += newUrl;
+				var end = text.Value.Length;
+
+				var style = new XElement(
+					gramps + "style",
+					new XAttribute("name", "link"),
+					new XAttribute("value", newUrl),
+					new XElement(gramps + "range",
+						new XAttribute("start", start),
+						new XAttribute("end", end)));
+
+				note.Add(style);
+
+				Console.WriteLine($"{oldUrl} -> {newUrl}");
 			}
 
 			return document;
